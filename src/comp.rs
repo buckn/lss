eaa53036c409527b45cd445e271530ce8ce9c71f
buckn <git@buckn.dev>
2021-11-10 10:57:05 -0500

    init

---
+++ src/comp.rs
@@ -0,0 +1,257 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+use glam::f32::{Vec2};
+use serde::{Deserialize, ser, Serialize};
+
+/*comp
+    Stat
+*/
+#[derive(Copy, Clone, Debug, Serialize, Deserialize, Default)]
+pub struct Stat {
+    pub el: El,
+    //multiplying or dividing factor when attacking or defending depending upon elements involved
+    pub el_deg: f32,
+    pub hp: f32,
+    //mag and spr create a damage multiplier of 1 when identical, and should be similar in closely powered enemies
+    pub mag: u32,
+    pub spr: u32,
+    //vit and atk create a damage multiplier of 1 when identical, and should be similar in closely powered enemies
+    pub vit: u32,
+    pub atk: u32,
+    pub spd: u32,
+}
+
+impl Stat {
+    pub fn hit(&mut self, other: &mut Stat, mag: bool, dmg_mod: f32) {
+        //subtract hp of other (the entity getting hit) by total damage of self (the attacker)
+        other.hp -= (({
+            //calculate magic or physical multiplier (with similar strength entities should be relatively close to 1)
+            if mag {
+                self.mag / other.spr
+            } else {
+                self.atk / other.vit
+            }
+        }) as f32 * {
+                    //if opponent weak against attack, multiply by own elemental degree
+                    if other.el.is_weak(self.el) {
+                        self.el_deg
+                    //if opponent is strong against attack, divide by opponents elemental degree
+                    } else {
+                        1.0 / other.el_deg
+                    }
+        }) * /* multiply by special damage modifier (this should be zero and in effect do nothing under normal circumstances) */ dmg_mod;
+    }
+}
+
+/*comp
+    El
+*/
+#[derive(Copy, Clone, Debug, Serialize, Deserialize, PartialEq)]
+pub enum El {
+    LAND,
+    SEA,
+    SKY
+}
+
+impl Default for El {
+    fn default() -> Self {
+        El::LAND
+    }
+}
+
+impl El {
+    pub fn is_weak(self, other: Self) -> bool {
+        ((self as u8 + (other as u8 - 1)) % 3) > 1
+    }
+
+    pub fn to_string(&self) -> &str {
+        use El::*;
+        match &self {
+            LAND => "🜃",
+            SKY => "🜁",
+            SEA => "🜄",
+        }
+    }
+}
+
+/*comp
+    Rig
+*/
+pub const Z_PUSH_REQ: f32 = 100.0;
+///A struct that describes the state of an objects movement,
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
+pub struct Rig {
+    pub mass: f32,
+    //position vector
+    pub pos: Vec2,
+    //z plane
+    pub z: u8,
+    //rotation
+    pub rot: f32,
+    //speed
+    pub max_speed: f32,
+    pub rot_speed: f32,
+    pub trans_speed: f32,
+    pub z_push: f32,
+    //acceleration
+    pub rot_acl: f32,
+    pub trans_acl: f32,
+    pub z_acl: f32,
+    ///vector that represents the current direction
+    pub dir: Vec2,
+    //radius of collisions
+    pub rad: f32,
+    //tether to another two rigs
+    pub tethers: [Option<Rc<Rig>>; 2],
+    //tether lerp amount
+    pub tether_lerp: f32,
+}
+
+impl Rig {
+    ///Exectes one physics tick for a Rig
+    pub fn tick(&mut self, tick_amnt: f32) {
+        println!("tick amnt {:?}", tick_amnt);
+        //ensure no negative acceleration
+        if self.z_acl < 0.0 {
+            self.z_acl = 0.0;
+        }
+
+        if self.rot_acl < 0.0 {
+            self.rot_acl = 0.0;
+        }
+
+        //accelerate
+        self.rot_speed += self.rot_acl * tick_amnt;
+        self.trans_speed += self.trans_acl * tick_amnt;
+        self.z_push += self.z_acl * tick_amnt;
+
+        //make sure the speed is not below zero
+        if self.trans_speed < 0.0 {
+            self.trans_speed = 0.0;
+        }
+        if self.trans_speed > self.max_speed {
+            self.trans_speed = self.max_speed;
+        }
+        //or the trans acl
+        if self.trans_acl < 0.0 {
+            self.trans_acl = 0.0;
+        }
+
+        if self.z_push < 0.0 {
+            self.z_push = 0.0;
+        }
+
+        if self.rot_speed < 0.0 {
+            self.rot_speed = 0.0;
+        }
+
+        //initialize delta vec
+        let mut d_vec = self.dir;
+
+        d_vec = glam::vec2(d_vec.x * self.trans_speed, d_vec.y * self.trans_speed);
+
+        //transform by delta vec
+        self.transform(d_vec, 0);
+
+        //lerp by tethers
+        for tether in &mut self.tethers {
+            match tether {
+                Some(rig) => Rc::<Rig>::make_mut(rig).lerp(self.pos, self.tether_lerp),
+                _ => return ,
+            }
+        }
+    }
+
+    pub fn lerp(&mut self, _other: Vec2, _amnt: f32) {
+        unimplemented!()
+    }
+    pub fn transform(&mut self, d_vec: Vec2, d_z: u8) {
+        self.pos += d_vec;
+        self.z += d_z;
+    }
+
+    pub fn rotate_toward(&mut self, toward: Vec2) {
+        let point_from_origin = Vec2::new(self.pos.x - toward.x, self.pos.y - toward.y);
+        self.rot = point_from_origin.y.atan2(point_from_origin.x);
+    }
+}
+
+/*comp
+    Inf
+*/
+#[derive(Copy, Clone, Debug, Serialize, Deserialize, Default)]
+pub struct Inf {
+    pub qtty: u8,
+    pub spawned: bool,
+    pub vis: bool,
+    pub col: bool,
+    pub phys: bool,
+    pub grab: bool,
+}
+
+/*comp
+    Inv
+*/
+#[derive(Debug, Clone)]
+pub struct Inv {
+    pub items: Vec<Thing>,
+    pub len: u8,
+}
+
+impl Default for Inv {
+    fn default() -> Self {
+        Self {
+            items: Vec::new(),
+            len: 100,
+        }
+    }
+}
+
+/*comp
+    Typ
+*/
+#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
+pub enum Typ {
+    PLAYER,
+    KNIFE,
+    SPITTER,
+}
+
+impl Default for Typ {
+    fn default() -> Self {
+        use Typ::*;
+        PLAYER
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Thing {
+    pub typ: Typ,
+    pub held: Inv,
+    pub equip: Inv,
+    pub inf: Inf,
+    pub stat: Stat,
+    pub rig: Rig,
+    pub col: f32,
+}
+
+#[derive(Debug, Serialize)]
+pub struct It {
+    typ: Typ,
+    inf: Inf,
+    stat: Stat,
+    rig: Rig,
+    col: f32,
+}
+
+impl From<Thing> for It {
+    fn from(thing: Thing) -> Self {
+        It {
+            typ: thing.typ,
+            inf: thing.inf,
+            stat: thing.stat,
+            rig: thing.rig,
+            col: thing.col,
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,179 @@
+mod comp;
+
+use glam::Vec2;
+use macroquad::prelude::{ get_time, Camera2D, screen_height, is_mouse_button_down, is_mouse_button_released, is_key_down, next_frame, set_default_camera, BLUE, screen_width, draw_circle, GRAY, BLACK, BROWN, set_camera, WHITE, clear_background, MouseButton, KeyCode };
+
+use comp::*;
+use Typ::*;
+
+const ZOOM: f32 = 10000.0;
+const FRIC_CONST: f32 = 0.9;
+const CAM_LERP: f32 = 0.01;
+
+#[macroquad::main("esk")]
+async fn main() {
+    let mut state = CliState::default();
+
+    use crate::El::*;
+
+    let player = Thing { 
+        typ: PLAYER, 
+        stat: Stat { 
+            el: LAND, 
+            el_deg: 1.0, 
+            hp: 10.0, 
+            mag: 1, 
+            spr: 1, 
+            vit: 1, 
+            atk: 1, 
+            spd:1, 
+        }, 
+        rig: Rig {
+            mass: 200.0,
+            max_speed: 50.0,
+            ..Rig::default()
+        },
+        ..Thing::default()
+    };
+
+    let knife = Thing { 
+        typ: KNIFE, 
+        stat: Stat { 
+            el: LAND, 
+            el_deg: 1.0, 
+            hp: 10.0, 
+            mag: 1, 
+            spr: 1, 
+            vit: 1, 
+            atk: 1, 
+            spd:1, 
+        }, 
+        rig: Rig {
+            mass: 200.0,
+            max_speed: 50.0,
+            ..Rig::default()
+        },
+        ..Thing::default()
+    };
+
+    state.player = state.world.len();
+
+    state.world.push(player);
+    state.world.push(knife);
+
+    loop {
+        clear_background(WHITE);
+
+        //controls process
+
+        state.cntrl = Cntrl::new();
+        state.ms_clock = get_time() * 1000.0;
+
+        state.world[state.player].rig.dir.y = 0.0;
+        state.world[state.player].rig.dir.x = 0.0;
+
+        if state.cntrl.forward {
+            state.world[state.player].rig.dir.y -= 1.0;
+        }
+        if state.cntrl.backward {
+            state.world[state.player].rig.dir.y += 1.0;
+        }
+        if state.cntrl.left {
+            state.world[state.player].rig.dir.x -= 1.0;
+        }
+        if state.cntrl.right {
+            state.world[state.player].rig.dir.x += 1.0;
+        }
+
+        if state.cntrl.forward || state.cntrl.backward || state.cntrl.left || state.cntrl.right {
+            state.world[state.player].rig.trans_acl += state.world[state.player].stat.spd as f32 / 222.125;
+        }
+
+        state.world[state.player].rig.trans_acl -= FRIC_CONST / state.world[state.player].rig.mass;
+
+        //apply friction
+
+        //physics process
+
+        for thing in &mut state.world {
+            thing.rig.tick(state.ms_clock as f32);
+        }
+
+        //render process
+
+        //move cam tgt
+
+        state.cam_tgt = state.cam_tgt.lerp(state.world[state.player].rig.pos, CAM_LERP);
+
+        set_camera(&Camera2D {
+            zoom: macroquad::math::vec2(1.0, -screen_width() / screen_height()) / ZOOM,
+            target: macroquad::math::vec2(state.cam_tgt.x, state.cam_tgt.y),
+            ..Default::default()
+        });
+        for thing in &state.world {
+            match thing.typ {
+                PLAYER => draw_circle(thing.rig.pos.x, thing.rig.pos.y, 250.0, BLACK),
+                KNIFE => draw_circle(thing.rig.pos.x, thing.rig.pos.y, 125.0, BROWN),
+                SPITTER => draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, BLUE),
+            }
+        }
+
+        //println!("trans speed  {:?}", state.world[state.player].rig.trans_speed);
+
+        //println!("x:    {:?}", state.world[state.player].rig.pos.x);
+        //println!("y:    {:?}", state.world[state.player].rig.pos.y);
+
+        set_default_camera();
+
+        next_frame().await
+    }
+}
+
+#[derive(Debug, Default)]
+struct CliState {
+    pub world: Vec<Thing>,
+    pub player: usize,
+    pub cntrl: Cntrl,
+    pub cam_tgt: Vec2,
+    pub ms_clock: f64,
+}
+
+#[derive(Debug, Default)]
+pub struct Cntrl {
+    forward: bool,
+    backward: bool,
+    left: bool,
+    right: bool,
+    rotl: bool,
+    rotr: bool,
+    pickup: bool,
+    drop: bool,
+    fire1_released: bool,
+    fire2_released: bool,
+    fire3_released: bool,
+    fire1_down: bool,
+    fire2_down: bool,
+    fire3_down: bool,
+}
+
+impl Cntrl {
+    pub fn new() -> Self {
+        use KeyCode::*;
+        Cntrl {
+            forward: is_key_down(W),
+            backward: is_key_down(S),
+            left: is_key_down(A),
+            right: is_key_down(D),
+            rotl: is_key_down(Q),
+            rotr: is_key_down(E),
+            pickup: is_key_down(F),
+            drop: is_key_down(G),
+            fire1_released: is_mouse_button_released(MouseButton::Left),
+            fire2_released: is_mouse_button_released(MouseButton::Right),
+            fire3_released: is_mouse_button_released(MouseButton::Middle),
+            fire1_down: is_mouse_button_down(MouseButton::Left),
+            fire2_down: is_mouse_button_down(MouseButton::Right),
+            fire3_down: is_mouse_button_down(MouseButton::Middle),
+        }
+    }
+}
